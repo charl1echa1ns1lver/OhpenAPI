d4057f6467d8f4584c8f784054704bffbffd3315
charl1echa1ns1lver <45205136+charl1echa1ns1lver@users.noreply.github.com>
2020-01-20 10:19:29 +0100

    Update TC03_WrongPostcode.java
    report.get().log instead of Log.logger

--- src/test/java/demo/TC03_WrongPostcode.java
+++ src/test/java/demo/TC03_WrongPostcode.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 import dto.ZopaUser;
 import framework.base.FrameworkProperties;
 import framework.base.WebDriverFacade;
@@ -23,16 +25,16 @@ public class TC03_WrongPostcode extends TestBase {
     	ZopaUser borrower = getBorrower();
     	String randomPostcode = "XXX" + RandomStringUtils.randomAlphanumeric(3);
     	borrower.setPostCode(randomPostcode);
-       	Log.testStep("Given a user with an incorrect postcode like > " + randomPostcode);
+	report.get().log(Status.INFO, "Given a user with an incorrect postcode like > " + randomPostcode);
     	ZopaHomePage home = WebDriverFacade.navigateTo(ZopaHomePage.class,FrameworkProperties.getBaseUrl());
     	home.clickAcceptAllCookies();
-    	Log.testStep("When navigating to Loans Page");
+    	report.get().log(Status.INFO, "When navigating to Loans Page");
     	LoansPage loans = home.clickGetPersonalizedRates();
     	loans.selectLoanYears(borrower.getYearsLoan());
-    	Log.testStep("And navigating to Personalized Rates Page");
+    	report.get().log(Status.INFO, "And navigating to Personalized Rates Page");
     	PersonalizedLoanRatesPage loanRates = loans.clickGetMyPersonalizedRates();
     	loanRates.clickAcceptAllCookies();
-    	Log.testStep("And entering all info on fields until reaching postcode");
+    	report.get().log(Status.INFO, "And entering all info on fields until reaching postcode");
     	loanRates.setLoanFor(borrower.getLoanFor());
     	loanRates.setEmail(borrower.getMail());
     	loanRates.setTitle(borrower.getTitle());
@@ -45,11 +47,11 @@ public class TC03_WrongPostcode extends TestBase {
     	loanRates.setEmploymentStatus(borrower.getEmploymentStatus());
     	loanRates.setAnnualIncome(borrower.getAnnualIncome());
     	loanRates.setHomeStatus(borrower.getHomeStatus());
-    	Log.testStep("When entering an invalid postcode");
+    	report.get().log(Status.INFO, "When entering an invalid postcode");
     	loanRates.setPostcode(borrower.getPostcode());
     	loanRates.clickLookUpAddress();
     	setBorrower(borrower);
-    	Log.testStep("Then an error message is displayed > 'Oops, this postcode doesn't look quite right");
+    	report.get().log(Status.INFO, "Then an error message is displayed > 'Oops, this postcode doesn't look quite right");
     	Assert.assertTrue(loanRates.isPostCodeErrorDisplayed());
     	Assert.assertEquals("Oops, this postcode doesn't look quite right", loanRates.getPostCodeErrorText());
     }
